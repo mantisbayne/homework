0a4730b46f5f0ba32421cdb3d061f406d876b945
mantisbayne <mrdthbyn@gmail.com>
2013-11-10 19:15:27 -0600

    hides and shows two images

---
+++ task02/scripts/task02.js
@@ -0,0 +1,19 @@
+$(function() {
+	$('#btn-hide').click(function() {
+		$('#cup img').hide('slow');
+	});
+
+	$('#btn-show').click(function() {
+		$('#cup img').show('slow');
+	});
+});
+
+$(function() {
+	$('#dad-hide').click(function() {
+		$('#dad img').hide('slow');
+	});
+
+	$('#dad-show').click(function() {
+		$('#dad img').show('slow');
+	});
+});
