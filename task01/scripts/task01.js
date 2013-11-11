ffb1c0fedcc494629a9df2d4e7cf9146a4b3cf91
mantisbayne <mrdthbyn@gmail.com>
2013-11-10 18:12:44 -0600

    pink button should immediately make page pink

--- task01/scripts/task01.js
+++ task01/scripts/task01.js
@@ -7,15 +7,11 @@ $(function() {
 
 	//
 	$('#pink-maker').click(function() {
-		r += 150;
-		g -= 150;
-		$('body').css('background-color', 'rgb(' + r + ', ' + g + ' , 255)'); 
+		$('body').css('background-color', 'rgb(200, 50, 90)'); 
+	});
 
 	$('#blue-maker').click(function() {
-		r -= 100;
-		g += 100;
-		$('body').css('background-color', 'rgb(' + r + ', ' + g + ', 255)');
-
-		});
+		$('body').css('background-color', 'rgb(0, 0, 100)');
 	});
+
 });
