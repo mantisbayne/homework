c28de1bdc2338508dad6d1503c8958ad00e875b5
mantisbayne <mrdthbyn@gmail.com>
2013-11-28 10:01:28 -0600

    brainstorming code ideas

--- task03/scripts/task03.js
+++ task03/scripts/task03.js
@@ -1,13 +1,25 @@
-$(function() {
+var rto-play = $(function() {
 	$("#rto-play").click(function() {
 		$('.cat-start').css('display', 'block');
 		$('#rto-play').hide('fast');
 	});
 })
 
-$(function() {
-	$("btn-explore").click(function() {
-		$('.cat-spy').css('display', 'block');
-			$('#rto-explore').hide('fast');
-	});			
-})
+if //user clicks button {
+	rto-play
+} else {
+	//pop up "You died"
+}
+
+var cat-explore = $(function() {
+					$("btn-explore").click(function() {
+						$('.cat-spy').css('display', 'block');
+						$('#rto-explore').hide('fast');
+					});			
+				})
+
+if //user clicks button {
+	cat-explore
+} else {
+	"You died"
+}
